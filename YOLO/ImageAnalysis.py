24dd8e6f07aeb8b941682461f87a98f163894029
kazuki kodashima <kodashima.kazuki@ebara.com>
2026-08-04 16:11:54 +0900

    20260804:台車配置エリアの検知

--- YOLO/ImageAnalysis.py
+++ YOLO/ImageAnalysis.py
@@ -1,17 +1,11 @@
+#画像分析簡易
 from ultralytics import YOLO
 
 # 学習済みモデルをロード
-model = YOLO(r"C:\YOLO\YOLO\best.pt")
+model = YOLO(r"C:\YOLO\YOLO\runs\detect\train-8\weights\best.pt")
 
-url = r"C:\Users\01A1561\Downloads\98996_640x360.mp4"
+url = r"C:\YOLOcamera\Camera01_20260804_132600.mp4"
 results = model(url,save=True)
 
-for frame_idx, result in enumerate(results):
-    boxes = result.boxes  # 検出結果のボックス情報
-    for box in boxes:
-        cls_id = int(box.cls[0])        # クラスID
-        cls_name = model.names[cls_id]  # クラス名
-        conf = float(box.conf[0])       # 信頼度 (0.0〜1.0)
-        print(f"フレーム {frame_idx}: 検出={cls_name}, 信頼度={conf:.2f}")
 
 print("分析完了")
